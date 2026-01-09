9773ff904e88d17114593997e24c22f70296f70a
Santeri Korri <santeri.korri@knowit.fi>
2026-01-09 12:34:03 +0200

    OPHSUPA-154 Lisätään untuvaopintopolku SAN

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ mvn dependency:copy -Dartifact=com.alibaba:dns-cache-manipulator:$CACHE_MANIPULA
 cp ./download/dns-cache-manipulator-$CACHE_MANIPULATOR_VERSION.jar ./dns.jar
 
 # luodaan self-signed -certti
-openssl req -x509 -newkey rsa:2048 -keyout docker/nginx/selfsigned-key.pem -out docker/nginx/selfsigned-cert.pem -days 3650 -nodes -subj "/CN=*.untuvaopintopolku.fi" -addext "subjectAltName=DNS:*.hahtuvaopintopolku.fi,DNS:*.testiopintopolku.fi"
+openssl req -x509 -newkey rsa:2048 -keyout docker/nginx/selfsigned-key.pem -out docker/nginx/selfsigned-cert.pem -days 3650 -nodes -subj "/CN=*.untuvaopintopolku.fi" -addext "subjectAltName=DNS:*.untuvaopintopolku.fi,DNS:*.hahtuvaopintopolku.fi,DNS:*.testiopintopolku.fi"
 
 # importataan self-signed -certti JVM ca-tiedostoon
 cp cacerts_original cacerts
